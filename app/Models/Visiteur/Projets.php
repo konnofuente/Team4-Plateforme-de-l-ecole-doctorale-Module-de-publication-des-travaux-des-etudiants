4897adf531b8f5bd1f89879d046f1a0476990555
ojong <82442816+ojongclinton@users.noreply.github.com>
2023-03-24 08:54:02 +0100

    working

---
+++ app/Models/Visiteur/Projets.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models\Visiteur;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Projets extends Model
+{
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+
+}
---
+++ resources/views/layouts/visitor/header.blade.php
@@ -0,0 +1,20 @@
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Gestion des Etudiants</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
+
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <style>
+        .specifie_collapase {
+            display: none;
+        }
+    </style>
+</head>
