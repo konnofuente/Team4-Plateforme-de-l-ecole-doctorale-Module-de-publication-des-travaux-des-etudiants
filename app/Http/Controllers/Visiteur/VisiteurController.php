45a7bd08fa62a8b627018af77c358d316e15ee02
konnofuente <konomelifuente@gmail.com>
2023-07-24 19:52:52 +0100

    ai and frondend

--- app/Http/Controllers/Visiteur/VisiteurController.php
+++ app/Http/Controllers/Visiteur/VisiteurController.php
@@ -235,160 +235,6 @@ public function extractTextById($projId)
 
 
 
-// public function aiAnalysis(Request $request)
-// {
-//     // Retrieve inputs from the request
-//     $projId = $request->input('projId');
-//     $prompt = $request->input('prompt');
-//     $resumeLang = $request->input('resumeLang');
-
-//     // Retrieve project information based on the project ID
-//     $selectedProject = Projets::find($projId);
-
-//     if (!$selectedProject) {
-//         return response()->json(['error' => 'Project not found'], 404);
-//     }
-
-//     // Extract relevant information to display on the AI side
-//     $projectInfo = [
-//         'theme' => $selectedProject->theme,
-//         'abstract' => $selectedProject->abstract,
-//         'language' => $selectedProject->language,
-//         // Add other relevant project information here
-//     ];
-
-//     // Set up Guzzle HTTP client
-//     $client = new Client();
-
-//     // Replace 'YOUR_API_KEY' with your actual GPT-3 API key
-//     $apiKey = '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';
-
-//     try {
-//         // Make a POST request to the GPT-3 API
-//         $response = $client->request('POST', 'https://api.openai.com/v1/chat/completions', [
-//             'headers' => [
-//                 'Content-Type' => 'application/json',
-//                 'Authorization' => 'Bearer ' . $apiKey,
-//             ],
-//             'json' => [
-//                 'model' => 'gpt-3.5-turbo',
-//                 'messages' => [
-//                     [
-//                         'role' => 'user',
-//                         'content' => $prompt,
-//                     ],
-//                 ],
-//             ],
-//         ]);
-
-//         // Process the API response
-//         $responseData = json_decode($response->getBody(), true);
-
-//         // Get the generated text from the API response
-//         $generatedText = $responseData['choices'][0]['message']['content'];
-
-//         // Create an array with all the data to be sent back to the frontend
-//         $responseData = [
-//             'projectInfo' => $projectInfo,
-//             'pdfText' => $this->extractTextById($projId),
-//             'summary' => $this->extractSummaryAlgo($pdfText),
-//             'generatedText' => $generatedText,
-//         ];
-
-//         // Return the JSON response
-//         return response()->json($responseData);
-//     } catch (\Exception $e) {
-//         // Handle API request error
-//         return response()->json(['error' => 'An error occurred while processing the request.'], 500);
-//     }
-// }
-
-
-// Controller
-
-// public function aiAnalysis(Request $request)
-// {
-//     // dd("yes");
-//     $projId = $request->input('projId');
-//     $prompt = $request->input('prompt');
-//     $resumeLang = $request->input('resumeLang');
-//     // Retrieve project information based on the project ID
-//     $selectedProject = Projets::find($projId);
-//     print($selectedProject);
-//     // echo($selectedProject);
-
-//     // dd($selectedProject);
-
-//     // if (!$selectedProject) {
-//     //     return view('visiteur.aiSide')->with('error', 'Project not found');
-//     // }
-
-//     // Extract relevant information to display on the AI side
-//     $projectInfo = [
-//         'theme' => $selectedProject->theme,
-//         'abstract' => $selectedProject->abstract,
-//         'language' => $selectedProject->language,
-//         // Add other relevant project information here
-//     ];
-//     $pdfText = $this->extractTextById($projId);
-    
-
-//     // Set up Guzzle HTTP client
-//     $client = new Client();
-
-//     // Replace 'YOUR_API_KEY' with your actual GPT-3 API key
-//     $apiKey = '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';
-
-//     // Prepare the API request data
-//     $requestData = [
-//         'prompt' => $prompt,
-//         // Additional options and parameters as needed for the GPT-3 API
-//     ];
-
-//     try {
-//         // Make a POST request to the GPT-3 API
-
-//         $response = $client->request('POST', 'https://api.openai.com/v1/chat/completions', [
-//             'headers' => [
-//                 'Content-Type' => 'application/json',
-//                 'Authorization' => $apiKey,
-//             ],
-//             'json' => [
-//                 'model' => 'gpt-3.5-turbo',
-//                 'messages' => [
-//                     [
-//                         'role' => 'user',
-//                         'content' => $prompt,
-//                     ],
-//                 ],
-    
-//             ],
-//         ]);
-
-//         // Process the API response
-//         $responseData = json_decode($response->getBody(), true);
-
-//         return $responseData;
-
-//         // Get the generated text from the API response
-//         // $generatedText = $responseData['choices'][0]['text'];
-        
-
-//         // Update the AI Side view with the generatedText and other data
-
-//         // return view('visiteur.aiSide')
-//         //     ->with('projectInfo', $projectInfo)
-//         //     ->with('pdfText', $this->extractTextById($projId))
-//         //     ->with('summary', $this->extractSummaryAlgo($pdfText))
-//         //     ->with('generatedText', $generatedText);
-//     } catch (\Exception $e) {
-//         // Handle API request 
-//         print($e);
-//         // return view('visiteur.aiSide')->with('error', 'An error occurred while processing the request.');
-//     }
-// }
-
-
 public function aiSearchMemoire(Request $request)
 {
     $projId = $request->input('projId');
@@ -491,10 +337,11 @@ public function extractMemoireText(Request $request)
     public function single_project($id)
     {
         $theProject = Projets::find($id);
+        $apiKey = env('OPENAI_API_KEY');
         if(!$theProject){
             return Redirect::route('visiteur.all');
         }
-        return view('visiteur.singleProject')->with('selected',$theProject);
+        return view('visiteur.singleProject')->with('selected',$theProject)->with('apikey',$apiKey);
     }
 
     public function createSecond(){
--- resources/views/visiteur/singleProject.blade.php
+++ resources/views/visiteur/singleProject.blade.php
@@ -1,3 +1,33 @@
+<style>
+    /* Add some basic styling to the chatbox messages */
+    #chatMessages {
+        margin-top: 10px;
+        overflow-y: auto;
+        max-height: 300px;
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+
+    .message {
+        margin-bottom: 10px;
+        padding: 5px 10px;
+        border-radius: 8px;
+        width: fit-content;
+        max-width: 70%;
+    }
+
+    .user {
+        background-color: #e6f7ff; /* Light blue for user messages */
+        float: left;
+    }
+
+    .ai {
+        background-color: #f0f0f0; /* Light gray for AI messages */
+        float: right;
+    }
+</style>
+
+
 @extends('layouts.visitor.body')
 @section('content')
     <div class="card">
@@ -5,6 +35,17 @@
             <p>Theme : <b> {{$selected->theme}} </b></p>
             <p> Date : <b>{{$selected->created_at}}</b></p>
         </div>
+        <script>
+            // Pass the selected to JavaScript variable
+            const selected = @json($selected);
+        </script>
+
+        <script>
+            // Pass the selected to JavaScript variable
+            const apikey =@json($apikey);
+        </script>
+
+
         <div class="card-body">
             <div> <!--Project Members-->
                 <p style="color:#0d6efd;">
@@ -47,6 +88,25 @@
             </div>
         </div>
 
+        <div id="chatbox" class="mt-4">
+            <h2 style="align-item:center" >COF AI</h2>
+            <div id="chatMessages"></div>
+            <div id="loadingSpinner" class="d-none">
+                <div class="spinner-border text-primary" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group">
+                    <label for="userInput">Your Message:</label>
+                    <textarea class="form-control" id="userInput" name="userInput" rows="4" placeholder="Enter your message here"></textarea>
+                </div>
+                <div class="form-group" style="padding:20px 0px;">
+                    <button type="button" id="sendBtn" class="btn btn-primary">Send</button>
+                </div>
+            </div>
+        </div>
+
         <div class="card-footer" style="display:flex;justify-content:space-between">
             <div>
                 <h4>ResearchHub</h4>
@@ -70,5 +130,113 @@
     <style>
 
     </style>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const chatbox = document.getElementById("chatbox");
+        const chatMessages = document.getElementById("chatMessages");
+        const userInput = document.getElementById("userInput");
+        const sendBtn = document.getElementById("sendBtn");
+
+        // Function to append a message to the chatbox
+
+        const loadingSpinner = document.getElementById("loadingSpinner");
+
+        function showLoadingSpinner() {
+            loadingSpinner.classList.remove("d-none");
+        }
+
+        function hideLoadingSpinner() {
+            loadingSpinner.classList.add("d-none");
+        }
+
+        function appendMessage(text, isUser = false) {
+            const messageDiv = document.createElement("div");
+            messageDiv.classList.add("message");
+            if (isUser) {
+                messageDiv.classList.add("user");
+            } else {
+                messageDiv.classList.add("ai");
+            }
+            messageDiv.textContent = text;
+            chatMessages.appendChild(messageDiv);
+            chatMessages.scrollTop = chatMessages.scrollHeight;
+        }
+
+        function sendMessageToAI(prompt, projId) {
+            // Use AJAX to send user input to the Laravel backend
+            prompt =  userInput.value ;
+
+            // Set up the GPT-3 API parameters
+            const gpt3ApiUrl = 'https://api.openai.com/v1/chat/completions';
+            const model = 'gpt-3.5-turbo';
+            const role = 'user';
+            // const apiKey = apiKey; // Replace with your actual GPT-3 API key
+
+            // Create the message object
+    //         const conversation = [
+    //             { role: "system", content: `your discussin with a user Base on the following information I will provide you: ${selected.abstract}` },
+    //             // { role: "system", content: `You are discussing the thesis with ID ${projId}.` },
+    //             { role: "user", content: prompt }
+    // ];
+
+    const conversation = [
+    { role: "system", content: `You are having a discussion about a research thesis on the topic: "${selected.theme}".` },
+    { role: "system", content: `The abstract of the thesis is as follows: "${selected.abstract}".` },
+    { role: "system", content: `The language of the thesis is: ${selected.language}.` },
+    { role: "system", content: `The author(s) of the thesis are: ${selected.author}.` },
+    { role: "system", content: `You can contact the researcher(s) at: ${selected.contact}.` },
+    { role: "user", content: prompt }
+];
+
+
+            const messages = [{
+                role: role,
+                content: prompt
+            }];
+
+            // Make a POST request to the GPT-3 API
+            fetch(gpt3ApiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${apikey}`
+                },
+                body: JSON.stringify({
+                    model: model,
+                    messages: conversation
+                }),
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                // Get the generated text from the API response
+                const generatedText = data.choices[0].message.content;
+                // Display AI response in the chatbox
+                appendMessage(generatedText, false);
+                hideLoadingSpinner();
+            })
+            .catch((error) => {
+                console.log("Error of Json :", error);
+                hideLoadingSpinner();
+            });
+        }
+
+        // Show the chatbox when a thesis is found
+        chatbox.style.display = "block";
+
+        // Event listener for the Send button (in the chatbox)
+        sendBtn.addEventListener("click", function() {
+            const prompt = userInput.value;
+            console.log("print");
+            appendMessage(prompt, true);
+            showLoadingSpinner(); // Show the loading spinner
+            sendMessageToAI(prompt, selected.projId);
+            userInput.value = "";
+        });
+    });
+</script>
+
+
 @endsection
 
